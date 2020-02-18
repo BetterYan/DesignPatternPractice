5b0f212aea608cfac0142c648c6e50a8ecd93c48
Zhuang Yan <wren_zhuang@foxmail.com>
2020-02-18 08:40:49 +0800

    bridge. TBD

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Dsign Pattern in DotNet
 
 The code shows the principles. Read the line one by one includes the comments just like read a book.
+
+The examples are written with C# 8.0
---
+++ Structural Patterns/Bridge/1.ConventionalBridge.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace Bridge
+{
+    public interface IRenderer
+    {
+        void RenderCircle(float radius);
+
+        void RenderSquare(float width, float height);
+
+        //etc...
+    }
+
+    public abstract class Shape
+    {
+        protected IRenderer renderer;
+
+        public Shape(IRenderer renderer)
+        {
+            this.renderer = renderer;
+        }
+
+        public abstract void Draw();
+
+        public abstract void Resize(float factor);
+    }
+
+    public class Circle : Shape
+    {
+        private float radius;
+
+        public Circle(IRenderer renderer, float radius) : base(renderer)
+        {
+            this.radius = radius;
+        }
+
+        public override void Draw()
+        {
+            renderer.RenderCircle(radius);
+        }
+
+        public override void Resize(float factor)
+        {
+            this.radius *= factor;
+        }
+    }
+
+    public class CircleRender : IRenderer
+    {
+        public void RenderCircle(float radius)
+        {
+            //Something to render the circle;
+        }
+
+        public void RenderSquare(float width, float height)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public static class Test
+    {
+        public static void Execute()
+        {
+            var circleRender = new CircleRender();
+            var circle = new Circle(circleRender, 5);
+            circle.Draw();
+            circle.Resize(2);
+            circle.Draw();
+        }
+
+        public static void ExecuteWithIoc()
+        {
+            //define the IoC globally somewhere
+            var cb = new ContainerBuilder();
+            cb.RegisterType<CircleRender>().As<IRenderer>();
+            cb.Register((c, p) => new Circle(c.Resolve<IRenderer>(), p.Positional<float>(0)));
+            using var c = cb.Build();
+
+            //take IoC into use
+            var circle = c.Resolve<Circle>(new PositionalParameter(0, 5.0f));
+            circle.Draw();
+            circle.Resize(2);
+            circle.Draw();
+            //We can see that the bridge pattern is no more than the application of the dependency inversion principle;
+            //The best approach to Bridge is still active avoidance, but if that is not possible,
+            //simply abstract away both hierarchies and find a way of connecting them.
+        }
+    }
+}
