76a66864ae032f8df30e91977758ca7285080726
Zhuang Yan <wren_zhuang@foxmail.com>
2020-02-18 14:08:34 +0800

    facade pattern

---
+++ Structural Patterns/Facade/NormalFacde.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    //We want to download the text from an Url
+    public static class Test
+    {
+        private static string url = @"http://www.google.com/robots.txt";
+
+        public static void NonFacade()
+        {
+            var request = WebRequest.Create(url);
+            request.Credentials = CredentialCache.DefaultCredentials;
+            using var response = request.GetResponse();
+            var dataStream = response.GetResponseStream();
+            using var reader = new StreamReader(dataStream);
+            var responseFromServer = reader.ReadToEnd();
+        }
+
+        public static void Facade()
+        {
+            var responseFromServer = new WebClient().DownloadString(url);
+        }
+    }
+}
---
+++ Structural Patterns/Facade/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Facade
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Facade is used to simplify the process which calls several complicated APIs with simple one.
+            //Facade put a simple interface in front of the complicated subsystems.
+        }
+    }
+}
