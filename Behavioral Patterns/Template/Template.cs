9d515dc0f5c5ff56dd9a3d5e954f2fd74299e7b2
Zhuang Yan <wren_zhuang@foxmail.com>
2020-02-19 14:56:00 +0800

    template pattern

---
+++ Behavioral Patterns/Template/Template.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template
+{
+    public abstract class Game
+    {
+        protected int currentPlayer;
+        protected readonly int numberOfPlayers;
+
+        public Game(int numberOfPlayers)
+        {
+            this.numberOfPlayers = numberOfPlayers;
+        }
+
+        protected abstract void Start();
+
+        protected abstract bool HaveWinner { get; }
+
+        protected abstract void TakeTurn();
+
+        protected abstract int WinningPlayer { get; }
+    }
+
+    public class Chess : Game
+    {
+        public Chess() : base(2)
+        {
+        }
+
+        private int maxTurns = 10;
+        private int turn = 1;
+
+        protected override bool HaveWinner => turn == maxTurns;
+
+        protected override int WinningPlayer => currentPlayer;
+
+        protected override void Start()
+        {
+            Console.WriteLine($"Starting a agame of chess with {numberOfPlayers} players.");
+        }
+
+        protected override void TakeTurn()
+        {
+            Console.WriteLine($"Turn {turn++} taken by player {currentPlayer}.");
+            currentPlayer = (currentPlayer + 1) % numberOfPlayers;
+        }
+    }
+}
