12e615687e256e03969b0338c0c08a7ccf655e5d
Zhuang Yan <wren_zhuang@foxmail.com>
2020-02-19 14:48:09 +0800

    strategy pattern

---
+++ Behavioral Patterns/Strategy/1.DynamicStrategy.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.DynamicStrategy
+{
+    public enum OutputFormat
+    {
+        Markdown,
+        Html
+    }
+
+    public interface IListStrategy
+    {
+        void Start(StringBuilder sb);
+
+        void AddListItem(StringBuilder sb, string item);
+
+        void End(StringBuilder sb);
+    }
+
+    public class TextProcessor
+    {
+        private StringBuilder sb = new StringBuilder();
+        private IListStrategy listStrategy;
+
+        public void AppendList(IEnumerable<string> items)
+        {
+            listStrategy.Start(sb);
+            foreach (var item in items)
+            {
+                listStrategy.AddListItem(sb, item);
+            }
+            listStrategy.End(sb);
+        }
+
+        public override string ToString()
+        {
+            return sb.ToString();
+        }
+    }
+
+    public class HtmlListStrategy : IListStrategy
+    {
+        public void Start(StringBuilder sb) => sb.Append("<ul>");
+
+        public void End(StringBuilder sb) => sb.AppendLine("</ul>");
+
+        public void AddListItem(StringBuilder sb, string item)
+        {
+            sb.AppendLine($"    <li>{item}</li>");
+        }
+    }
+}
---
+++ Behavioral Patterns/Strategy/2.StaticStrategy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.StaticStrategy
+{
+    public class TextProcessor<LS> where LS : Strategy.DynamicStrategy.IListStrategy, new()
+    {
+        private StringBuilder sb = new StringBuilder();
+        private DynamicStrategy.IListStrategy listStrategy = new LS();
+
+        public void AppendList(IEnumerable<string> items)
+        {
+            listStrategy.Start(sb);
+            foreach (var item in items)
+            {
+                listStrategy.AddListItem(sb, item);
+            }
+            listStrategy.End(sb);
+        }
+    }
+}
